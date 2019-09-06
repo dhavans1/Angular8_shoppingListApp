5254adce78efb4f537f7d0ca63457f5e9c85d0cd
dhavans1 <dhavan.shekarappa@gmail.com>
2019-09-06 18:25:35 -0400

    Structuring Recipe and Shopping List

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -9,10 +9,14 @@ import { recipe } from './recipe/recipe.model';
 
 export class recipeListComponent{
     recipeList: recipe[] = [];
+    Recipe: recipe;
 
     constructor(){
         this.recipeList.push(new recipe("Puliogare", "Tamarind Rice", "https://eastern.in/wp-content/uploads/2017/10/Puliogare-with-Instant-Puliogare-Mix-500x500.jpg", this.recipeList),
         new recipe("Chitranna", "Lemon Rice", "https://www.indianhealthyrecipes.com/wp-content/uploads/2018/06/lemon-rice-recipe.jpg", this.recipeList),
         new recipe("Bisi Bele Bath", "Lentil Masala Rice", "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Bisi_Bele_Bath_%28Bisibelebath%29.JPG/1200px-Bisi_Bele_Bath_%28Bisibelebath%29.JPG", this.recipeList))
+
+        this.Recipe = new recipe("Puliogare", "Tamarind Rice", "https://eastern.in/wp-content/uploads/2017/10/Puliogare-with-Instant-Puliogare-Mix-500x500.jpg", this.recipeList)
+
     }
 }
